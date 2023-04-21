7ba73f6fe4f0e8ad703802560b9239a1167066a8
thejas98 <thejasbharadwaj98@gmail.com>
2023-04-21 13:34:27 -0400

    Data Ingestion

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,15 +1,11 @@
-import requests
-import boto3
 
-# Download the dataset
-url = "https://data.cityofnewyork.us/resource/ipu4-2q9a.json"
-response = requests.get(url)
-data = response.content
+import pandas as pd
 
-# Upload the file to S3
-bucket_name = "permit-issuance"
-filename = "raw.json"
+# Set the API Endpoint URL
+url = "https://data.cityofnewyork.us/api/views/ipu4-2q9a/rows.csv?accessType=DOWNLOAD"
 
-s3 = boto3.resource("s3")
-bucket = s3.Bucket(bucket_name)
-bucket.put_object(Key=filename, Body=data)
+# Read the dataset from the URL
+df = pd.read_csv(url)
+
+# Save the dataset to a CSV file
+df.to_csv("data/DOB_Permit_Issuance.csv", index=False)
